5a90f2bc3f55bcbce85265c3646f0357494803c5
Anthony Brown <abrownie993@gmail.com>
2018-12-30 16:15:40 -0500

    Add files via upload

---
+++ Set-VMKernelAdapterVmotionSettings.ps1
@@ -0,0 +1,11 @@
+﻿$vmhost = Read-Host "What vmhost?"
+
+$ip = Read-Host "What IP address?"
+
+$sm = Read-Host "What subnet mask?"
+
+$mtu = Read-Host "What MTU size?"
+
+$vmk = Read-Host "What VMK Adapter?"
+
+Get-VMHostNetworkAdapter -VMHost $vmhost | Where-Object -Property Name -Match $vmk | Set-VMHostNetworkAdapter -IP $ip -SubnetMask $sm -Mtu $mtu -VMotionEnabled $true
